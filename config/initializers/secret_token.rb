c2028590f284ef7e83550f9a8eb539eedf0e1947
Anatolii <anatoliy.ruchka@gmail.com>
2012-08-20 21:12:20 +0300

    no Cedar-supported app detected

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '1c35fc680c9b8744fed6c050d5b6f2bc860fdffc2fd6405b7f4b6f16b0ae91230d83f1a638e63bb7e88945398647431cb542858c03893f93fce887f038cdab7f'
